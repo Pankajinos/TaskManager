1e60d0f98f160dfc6a8bba1ef1f0b6ef6392aabb
Pankaj Soni <pankajinos@gmail.com>
2025-03-10 01:51:35 +0530

    id fixing

--- frontend/src/components/createTodo.jsx
+++ frontend/src/components/createTodo.jsx
@@ -2,14 +2,13 @@ import { useState } from "react"
 import { Todos } from "./Todos";
 import '../App.css'
 
-const date = new Date();
 
 export function CreateTodo({ setTodo }) {
     const [title, setTitle] = useState("");
     return <div>
         <input type="text" id="heading" placeholder="Heading" onChange={(e) => { setTitle(e.target.value) }} />
         <button onClick={() => {
-            fetch(`${import.meta.env.VITE_BACKEND_BASEURL}/todo`, { method: "POST", body: JSON.stringify({ title: title, key: JSON.stringify(date.getMilliseconds()) }), headers: { "Content-Type": "application/json" } })
+            fetch(`${import.meta.env.VITE_BACKEND_BASEURL}/todo`, { method: "POST", body: JSON.stringify({ title: title, key: JSON.stringify(Date.now()) }), headers: { "Content-Type": "application/json" } })
                 .then(async (res) => {
                     if (!res.ok) {
                         const json = await res.json();
